ead97201f6a497cf653dad6ffe4a896daa825307
abhaysy <abhaysy80@gmail.com>
2025-09-28 07:07:03 +0530

    resume change

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -45,7 +45,7 @@ const About = () => {
 
         {/* Resume Button */}
         <a
-          href="https://drive.google.com/file/d/1bwyIfVB7qUrETfl-h_sTkKBuo2t3p_7v/view?usp=drive_link"
+          href="https://drive.google.com/file/d/1khTsIvHB-M_kgHc9zPwaiT0m7SVEGDsp/view?usp=drive_link"
           target="_blank"
           rel="noopener noreferrer"
           className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
